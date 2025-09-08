247634bdbf18eed524284093ebfe6a812a29b2ed
kenjimattos <70449934+kenjimattos@users.noreply.github.com>
2025-09-08 11:16:11 -0300

    refactor: Implementação de ambientes de desenvolvimento
    - Criação das variáveis de ambiente
    - Indexação do supabase às variaveis de ambiente
    - Indexação das chamadas de API às variáveis de ambiente

--- lib/backend/supabase/supabase.dart
+++ lib/backend/supabase/supabase.dart
@@ -4,9 +4,8 @@ import '/flutter_flow/flutter_flow_util.dart';
 export 'database/database.dart';
 export 'storage/storage.dart';
 
-String _kSupabaseUrl = 'https://hxgbaruenomkfeeafmff.supabase.co';
-String _kSupabaseAnonKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4Z2JhcnVlbm9ta2ZlZWFmbWZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4MTUzMjgsImV4cCI6MjA1OTM5MTMyOH0.V5jZLBEDXWfg2UsasoHQ6SDGIcDJLFIWBKlK8FBDows';
+String _kSupabaseUrl = FFDevEnvironmentValues().supabaseUrl;
+String _kSupabaseAnonKey = FFDevEnvironmentValues().supabaseAnonKey;
 
 class SupaFlow {
   SupaFlow._();
