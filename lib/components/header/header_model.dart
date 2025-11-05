4b50c817bd0827ecb9dbe81a46aae0a3c4fe448b
kenjimattos <70449934+kenjimattos@users.noreply.github.com>
2025-11-05 11:46:54 -0300

    feat: Redesign de interface - Header com contagem de plantões

--- lib/components/header/header_model.dart
+++ lib/components/header/header_model.dart
@@ -1,23 +1,19 @@
+import '/auth/supabase_auth/auth_util.dart';
 import '/backend/supabase/supabase.dart';
-import '/flutter_flow/flutter_flow_drop_down.dart';
+import '/components/loading/header_loading/header_loading_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import '/flutter_flow/form_field_controller.dart';
 import 'dart:ui';
+import '/flutter_flow/custom_functions.dart' as functions;
 import '/index.dart';
 import 'header_widget.dart' show HeaderWidget;
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class HeaderModel extends FlutterFlowModel<HeaderWidget> {
-  ///  State fields for stateful widgets in this component.
-
-  // State field(s) for DropDown widget.
-  String? dropDownValue;
-  FormFieldController<String>? dropDownValueController;
-
   @override
   void initState(BuildContext context) {}
 
